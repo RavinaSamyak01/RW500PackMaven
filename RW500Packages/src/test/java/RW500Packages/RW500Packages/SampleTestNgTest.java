bf84cfb07fb6c0a4d6ecf658d914ef532c9f91a8
rprajapati <rprajapati@SIPL92.SIPL.COM>
2022-08-04 11:52:51 +0530

    Convert TestNG into Maven Project

---
+++ RW500Packages/src/test/java/RW500Packages/RW500Packages/SampleTestNgTest.java
@@ -0,0 +1,25 @@
+package RW500Packages.RW500Packages;
+
+import org.openqa.selenium.support.PageFactory;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import RW500Packages.RW500Packages.pages.HomePage;
+
+public class SampleTestNgTest extends TestNgTestBase {
+
+  private HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
